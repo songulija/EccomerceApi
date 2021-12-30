f7cc5c4c697a4931082a55bdab0e2840aff9e64e
songulija <65647664+songulija@users.noreply.github.com>
2021-12-30 23:27:40 +0200

    Added all tables to DatabaseContext

--- EccomerceApi/Startup.cs
+++ EccomerceApi/Startup.cs
@@ -1,7 +1,9 @@
+using EccomerceApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace EccomerceApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("lukasConnection")));
             // adding Cors policy. so user from other networks could access our API. just adding policy with name
             //basically allowing here anybody and everybody to access this API
             services.AddCors(o =>
