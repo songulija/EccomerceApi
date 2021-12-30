3d657fbdc2997964cf51c4c68d19138478f03334
songulija <65647664+songulija@users.noreply.github.com>
2021-12-30 22:33:40 +0200

    Added Category and ProductCategory models

---
+++ EccomerceApi/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EccomerceApi.Models
+{
+    public class Category : BaseModel
+    {
+        //id of parent category
+        public int ParentId { get; set; }
+        public string Title { get; set; }
+        public string MetaTitle { get; set; }
+        public string Slug { get; set; }
+        public string Content { get; set; }
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+    }
+}
