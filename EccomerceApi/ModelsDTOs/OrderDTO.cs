45705ec0aabd0497646a5defae7b6890a9818ddd
songulija <65647664+songulija@users.noreply.github.com>
2022-01-01 23:19:26 +0200

    Added CategoryDTO, CommentDTO, OrderDTO

---
+++ EccomerceApi/ModelsDTOs/OrderDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EccomerceApi.ModelsDTOs
+{
+    public class CreateOrderDTO
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public double Tax { get; set; }
+        [Required]
+        public double ShippingPrice { get; set; }
+        public double Subtotal { get; set; }
+        //without vat
+        public double SubTotal { get; set; }
+        //with vat
+        public double Total { get; set; }
+        // paypal,stripe etc.
+        public string PaymentMethod { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime PaidAt { get; set; }
+        public bool IsDelivered { get; set; }
+        //it can be delivered later on
+        public DateTime? DeliveredAt { get; set; }
+    }
+    public class UpdateOrderDTO : CreateOrderDTO
+    {
+    }
+    public class OrderDTO : CreateOrderDTO
+    {
+        public int Id { get; set; }
+        public UserDTO User { get; set; }
+        public virtual IList<OrderItemDTO> OrderItems { get; set; }
+        public virtual IList<PaymentDTO> Payments { get; set; }
+        public virtual IList<ShipmentDTO> Shipments { get; set; }
+    }
+}
