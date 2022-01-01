9a440abc55003c77e79c610648fc3a94bdc5b7dd
songulija <65647664+songulija@users.noreply.github.com>
2022-01-01 23:20:35 +0200

    Added TagDTO, UserDTO, UserTypeDTO

---
+++ EccomerceApi/ModelsDTOs/UserTypeDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EccomerceApi.ModelsDTOs
+{
+    public class CreateUserTypeDTO
+    {
+        [Required]
+        public string Title { get; set; }
+    }
+    public class UpdateUserTypeDTO : CreateUserTypeDTO
+    {
+    }
+    public class UserTypeDTO : CreateUserTypeDTO
+    {
+        public int Id { get; set; }
+        public virtual IList<UserDTO> Users { get; set; }
+    }
+}
