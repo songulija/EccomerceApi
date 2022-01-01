cedd2b6e16990a99ff9ffac1b1e60968b9723c8c
songulija <65647664+songulija@users.noreply.github.com>
2022-01-01 23:19:48 +0200

    Added OrderItemDTO, PaymentDTO, ProductCategoryDTO

---
+++ EccomerceApi/ModelsDTOs/ProductCategoryDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EccomerceApi.ModelsDTOs
+{
+    public class CreateProductCategoryDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+    }
+    public class UpdateProductCategoryDTO : CreateProductCategoryDTO
+    {
+    }
+    public class ProductCategoryDTO : CreateProductCategoryDTO
+    {
+        public int Id { get; set; }
+        public ProductDTO Product { get; set; }
+        public CategoryDTO Category { get; set; }
+    }
+}
